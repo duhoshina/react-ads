1f9fb36317f9654ee1640c6a2e1da45ae786962d
Luis Eduardo Hoshina <duhoshina@gmail.com>
2025-09-13 20:10:26 -0300

    feat: added .gitignore

--- dist/types/hooks/useAdService.d.ts
+++
@@ -1,3 +0,0 @@
-import AdService from '../lib/AdService';
-import type { AdConfig } from '../types/ads';
-export declare const useAdService: (config: AdConfig) => AdService | undefined;
--- dist/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { default as AdService } from './lib/AdService';
-export { AdProvider, useAd } from './components/AdProvider';
-export { default as AdSlot } from './components/AdSlot';
-export { useAdService } from './hooks/useAdService';
-export { BidderPresets } from './utils/bidders';
-export type { AdConfig, AdSlot as AdSlotType, AdUnit, PrebidBidder } from './types/ads';
-export declare const VERSION = "1.0.0";
