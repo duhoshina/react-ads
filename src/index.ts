517030a2bc2fe885e212ee10c70c994a3519ad84
Luis Eduardo Hoshina <duhoshina@gmail.com>
2025-09-13 20:15:32 -0300

    refactor: removed logs on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-// Exportar tudo que o usuário vai precisar
 export { default as AdService } from './lib/AdService';
 export { AdProvider, useAd } from './providers/AdProvider';
 export { default as AdSlot } from './components/AdSlot';
 export { useAdService } from './hooks/useAdService';
 export { BidderPresets } from './utils/bidders';
 
-// Exportar tipos
 export type {
   AdConfig,
   AdSlot as AdSlotType,
@@ -13,5 +11,4 @@ export type {
   PrebidBidder
 } from './types/ads';
 
-// Versão da lib
 export const VERSION = '1.0.0';
